2aee929852b9184576a97c4673cbf96e1960f107
Finomnis <finomnis@gmail.com>
2021-11-13 18:22:49 +0100

    Add usb example

---
+++ src/bin/usb.rs
@@ -0,0 +1,62 @@
+#![no_main]
+#![no_std]
+
+use test_nrf52840 as _; // global logger + panicking-behavior + memory layout
+use test_nrf52840::hal;
+
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+use hal::{
+    usbd::{UsbPeripheral, Usbd},
+    Clocks,
+};
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let periph = hal::pac::Peripherals::take().unwrap();
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+
+    let usb_bus = Usbd::new(UsbPeripheral::new(periph.USBD, &clocks));
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .max_packet_size_0(64) // (makes control transfers 8x faster)
+        .build();
+
+    loop {
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    defmt::info!("Received {=u32}", *c as u32);
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+}
